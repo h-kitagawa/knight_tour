ef0899181dc7d55422181d7a8163542f9fca23b6
Hironori Kitagawa <h_kitagawa2001@yahoo.co.jp>
2015-05-04 18:55:08 +0900

    Updated knight2.c

--- kitagawa/knight2.c
+++ kitagawa/knight2.c
@@ -3,50 +3,63 @@
 #define SIZE 6
 #define BIT(a) (1ULL<<(a))
 typedef unsigned long long int ULL;
+#define PACK(a,b) (((a)<<16)+b)
 
 ULL total = 0;
-typedef struct {
-  unsigned eh: 8;
-  signed   v:  4;
-} hv_store;
 
-const unsigned int rightmost = 1<<(SIZE-1);
-const int topmost = SIZE*(SIZE-1);
-const ULL end_flag =  BIT((SIZE+1)*(SIZE-1));
-const ULL full = BIT(SIZE*SIZE)-1ULL;
-
-const ULL se_stuck = ( BIT(2*SIZE-3) + BIT(3*SIZE-2) );
-const ULL nw_stuck = ( BIT(SIZE*(SIZE-2)+2) + BIT(SIZE*(SIZE-3)+1) );
+#define RIGHTMOST (1<<(SIZE-1))
+#define TOPMOST   (1<<((SIZE-1)*(SIZE-1)))
+#define FULL      (BIT(SIZE*SIZE)-1ULL)
+const ULL end_flag = BIT((SIZE+1)*(SIZE-1));
+const ULL ne_stuck = BIT(SIZE*(SIZE-2)+SIZE-3) + BIT(SIZE*(SIZE-3)+(SIZE-2)) ;
 
 #define check_and_rec(nh, dv) \
-    {if ( v+dv<SIZE*SIZE ) { const ULL s = ((ULL)nh)<<(v+dv);\
-      if ( ! (state&s) ) main_loop(nh, v+dv, state|s);}} \
-    {if ( v-dv>=0 ) {        const ULL s = ((ULL)nh)<<(v-dv);\
-      if ( ! (state&s) ) main_loop(nh, v-dv, state|s);}}
+    {if ( v<SIZE*SIZE-dv ) \
+      if ( ! (state&(((ULL)nh)<<(v+dv))) ) \
+	main_loop( PACK(nh, v+dv), state|(((ULL)nh)<<(v+dv)));}; \
+    {if ( v>=dv ) \
+      if ( ! (state&(((ULL)nh)<<(v-dv))) ) \
+	main_loop( PACK(nh, v-dv), state|(((ULL)nh)<<(v-dv)));};
 
-void main_loop(const unsigned int eh, const int v, const ULL state) {
-  if ( state & end_flag ) {
-      if ( state==full ) { total+=2; printf("%lld\n", total); }
+void main_loop(const unsigned int p, const ULL state) {
+  const unsigned int eh = p>>16;
+  const unsigned int v =  p&0xffff;
+  if ( state & end_flag ) { // reached the goal
+      if ( state==FULL ) { total+=2; printf("%lld\n", total); }
+  } else if ( (state & ne_stuck) == ne_stuck) { // cannot reach the goal
+    return;
   } else {
-    const ULL sa = ((ULL)eh)<<v;
-    if (eh <rightmost/2 ) {
+    switch(eh) {
+    case 1:
+      check_and_rec(2, 2*SIZE); 
+      check_and_rec(4, SIZE);
+      break;
+    case 2:
+      check_and_rec(1, 2*SIZE);
+      check_and_rec(4, 2*SIZE); 
+      check_and_rec(8, SIZE);
+      break;
+    case RIGHTMOST:
+      check_and_rec(RIGHTMOST>>1, 2*SIZE); 
+      check_and_rec(RIGHTMOST>>2, SIZE);
+      break;
+    case RIGHTMOST/2:
+      check_and_rec(RIGHTMOST, 2*SIZE); 
+      check_and_rec(RIGHTMOST>>2, 2*SIZE); 
+      check_and_rec(RIGHTMOST>>3, SIZE);
+      break;
+    default:
+      check_and_rec(eh>>1, 2*SIZE);
       check_and_rec(eh<<1, 2*SIZE); 
       check_and_rec(eh<<2, SIZE);
-    } else if (eh < rightmost) { 
-      check_and_rec(eh<<1, 2*SIZE); 
-    } 
-    if (eh>>2) {
-      check_and_rec(eh>>1, 2*SIZE);
       check_and_rec(eh>>2, SIZE);
-    } else if (eh>>1) {
-      check_and_rec(eh>>1, 2*SIZE);
-    }
+     }
   }
 }
 
 int main(void) {
   printf("size %d\n", SIZE);
-  main_loop(4,SIZE,1ULL+BIT(SIZE+2));
+  main_loop( PACK (4,SIZE) ,1ULL+BIT(SIZE+2));
   printf("total %lld\n", total);
 }
 
