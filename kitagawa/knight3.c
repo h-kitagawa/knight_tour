97ff093502e8effdc79d678ec09580464ddb582a
Hironori Kitagawa <h_kitagawa2001@yahoo.co.jp>
2015-05-05 22:33:46 +0900

    knight3.c: added pruning

--- kitagawa/knight3.c
+++ kitagawa/knight3.c
@@ -8,9 +8,13 @@
 #define BIT(a) (1ULL<<(a))
 typedef unsigned long long int ULL;
 
-#define RIGHTMOST (1<<(SIZE-1))
+#define RIGHTMOST (1ULL<<(SIZE-1))
 #define FULL      (BIT(SIZE*SIZE)-1ULL)
 const ULL end_flag = BIT((SIZE+1)*(SIZE-1));
+const ULL nwcorner_flag = BIT(SIZE*(SIZE-1));
+const ULL secorner_flag = BIT(SIZE-1);
+const ULL ne_sww_flag = BIT((SIZE-2)*SIZE+(SIZE-3));
+const ULL ne_ssw_flag = BIT((SIZE-3)*SIZE+(SIZE-2));
 
 
 void main_loop(const unsigned int eh, const unsigned int v, const ULL state, ULL* sum);
@@ -21,48 +25,78 @@ void inline check_and_rec(const ULL nh, const unsigned int nv, const ULL state, 
 
 void main_loop(const unsigned int eh, const unsigned int v, const ULL state, ULL* sum) {
   if ( state & end_flag ) { // reached the goal
-      if ( state==FULL ) {
-	printf("%lld\n", ++(*sum));
-      }
+      if ( state==FULL ) { printf("%lld\n", ++(*sum)); }
   } else {
     switch(eh) {
     case 1:
-      check_and_rec(2, v+2*SIZE, state, sum); 
-      check_and_rec(2, v-2*SIZE, state, sum); 
+      check_and_rec(2, v+2*SIZE, state, sum);
+      check_and_rec(2, v-2*SIZE, state, sum);
       check_and_rec(4, v-  SIZE, state, sum);
       check_and_rec(4, v+  SIZE, state, sum);
       break;
     case 2:
-      check_and_rec(1, v+2*SIZE, state, sum);
-      check_and_rec(4, v+2*SIZE, state, sum); 
-      check_and_rec(4, v-2*SIZE, state, sum); 
-      check_and_rec(1, v-2*SIZE, state, sum);
-      check_and_rec(8, v+  SIZE, state, sum);
-      check_and_rec(8, v-  SIZE, state, sum);
+       if ((v==(SIZE-3)*SIZE) && !(state & nwcorner_flag)) {// nwcorner check
+	 check_and_rec(4, (SIZE-2)*SIZE, state | nwcorner_flag,  sum);
+       } else {
+	 check_and_rec(1, v+2*SIZE, state, sum);
+	 check_and_rec(4, v+2*SIZE, state, sum);
+	 check_and_rec(4, v-2*SIZE, state, sum);
+	 check_and_rec(1, v-2*SIZE, state, sum);
+	 check_and_rec(8, v+  SIZE, state, sum);
+	 check_and_rec(8, v-  SIZE, state, sum);
+       }
       break;
     case RIGHTMOST:
-      check_and_rec(RIGHTMOST>>1, v+2*SIZE, state, sum); 
-      check_and_rec(RIGHTMOST>>1, v-2*SIZE, state, sum); 
+      check_and_rec(RIGHTMOST>>1, v+2*SIZE, state, sum);
+      check_and_rec(RIGHTMOST>>1, v-2*SIZE, state, sum);
       check_and_rec(RIGHTMOST>>2, v-  SIZE, state, sum);
       check_and_rec(RIGHTMOST>>2, v+  SIZE, state, sum);
       break;
     case RIGHTMOST>>1:
-      check_and_rec(RIGHTMOST,    v+2*SIZE, state, sum); 
-      check_and_rec(RIGHTMOST>>2, v+2*SIZE, state, sum); 
-      check_and_rec(RIGHTMOST>>2, v-2*SIZE, state, sum); 
-      check_and_rec(RIGHTMOST,    v-2*SIZE, state, sum); 
-      check_and_rec(RIGHTMOST>>3, v+  SIZE, state, sum);
-      check_and_rec(RIGHTMOST>>3, v-  SIZE, state, sum);
+       if ((v==2*SIZE) && !(state & secorner_flag)) {// secorner check
+	 check_and_rec(RIGHTMOST>>2, SIZE, state | secorner_flag,  sum);
+       } else if ((v==(SIZE-3)*SIZE) && (state & ne_sww_flag)) {
+	 // we have to reach the goal at the next move
+	 if ( state==FULL-end_flag ) { printf("%lld\n", ++(*sum)); }
+       } else {
+	 check_and_rec(RIGHTMOST,    v+2*SIZE, state, sum);
+	 check_and_rec(RIGHTMOST>>2, v+2*SIZE, state, sum);
+	 check_and_rec(RIGHTMOST>>2, v-2*SIZE, state, sum);
+	 check_and_rec(RIGHTMOST,    v-2*SIZE, state, sum);
+	 check_and_rec(RIGHTMOST>>3, v+  SIZE, state, sum);
+	 check_and_rec(RIGHTMOST>>3, v-  SIZE, state, sum);
+       }
+      break;
+    case RIGHTMOST>>2:
+      if ((v==SIZE) && !(state & secorner_flag)) {// secorner check
+	check_and_rec(RIGHTMOST>>1, 2*SIZE, state | secorner_flag,  sum);
+      } else if ((v==(SIZE-2)*SIZE) && (state & ne_ssw_flag)) {
+	 // we have to reach the goal at the next move
+	 if ( state==FULL-end_flag ) { printf("%lld\n", ++(*sum)); }
+      } else {
+	check_and_rec(RIGHTMOST>>1, v+2*SIZE, state, sum);
+	check_and_rec(RIGHTMOST>>3, v+2*SIZE, state, sum);
+	check_and_rec(RIGHTMOST>>3, v-2*SIZE, state, sum);
+	check_and_rec(RIGHTMOST>>1, v-2*SIZE, state, sum);
+	check_and_rec(RIGHTMOST>>4, v+  SIZE, state, sum);
+	check_and_rec(RIGHTMOST,    v+  SIZE, state, sum);
+	check_and_rec(RIGHTMOST,    v-  SIZE, state, sum);
+	check_and_rec(RIGHTMOST>>4, v-  SIZE, state, sum);
+      }
       break;
     default: // この中で switch しない方が速い
-      check_and_rec(eh>>1, v+2*SIZE, state, sum);
-      check_and_rec(eh<<1, v+2*SIZE, state, sum); 
-      check_and_rec(eh<<1, v-2*SIZE, state, sum); 
-      check_and_rec(eh>>1, v-2*SIZE, state, sum);
-      check_and_rec(eh>>2, v+  SIZE, state, sum);
-      check_and_rec(eh<<2, v+  SIZE, state, sum);
-      check_and_rec(eh<<2, v-  SIZE, state, sum);
-      check_and_rec(eh>>2, v-  SIZE, state, sum);
+      if ((eh==4) && (v==(SIZE-2)*SIZE) && !(state & nwcorner_flag)) {// nwcorner check
+	check_and_rec(2, (SIZE-3)*SIZE, state | nwcorner_flag,  sum);
+      } else {
+	check_and_rec(eh>>1, v+2*SIZE, state, sum);
+	check_and_rec(eh<<1, v+2*SIZE, state, sum);
+	check_and_rec(eh<<1, v-2*SIZE, state, sum);
+	check_and_rec(eh>>1, v-2*SIZE, state, sum);
+	check_and_rec(eh>>2, v+  SIZE, state, sum);
+	check_and_rec(eh<<2, v+  SIZE, state, sum);
+	check_and_rec(eh<<2, v-  SIZE, state, sum);
+	check_and_rec(eh>>2, v-  SIZE, state, sum);
+      }
     }
   }
 }
@@ -102,7 +136,7 @@ int main(void) {
 
   for (int i=0;i<5;i++) {
     gt += sum[i]<<1;
-    printf("%c: sum %lld\n", '@'+i, sum[i]<<1);
+    printf("%c: sum %lld\n", 'A'+i, sum[i]<<1);
   }
   printf("total %lld\n", gt);
 }
--- kitagawa/knight3_build.sh
+++ kitagawa/knight3_build.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+set_START() {
+        local dummy
+        read START dummy < /proc/uptime
+}
+
+get_ELAPS() {
+        local dummy
+        read END dummy < /proc/uptime
+        let ELAPS=${END/./}0-${START/./}0
+}
+
 gcc ${CFLAGS} -g  -o knight3_5 -DSIZE=5 knight3.c -lpthread
 TEST=`./knight3_5 |tail -1 | sed 's/total //'`
 if [[ "$TEST" -ne 56  ]]; then
@@ -9,6 +20,15 @@ fi
 #rm knight3_5
 gcc ${CFLAGS} -s -Ofast -o knight3_6 -DSIZE=6 knight3.c -lpthread
 gcc ${CFLAGS} -s -Ofast -S -DSIZE=6 knight3.c # assembly
-TEST=`(time ./knight3_6  ) 2>&1 >/dev/null| tail -n3 | head -n1|sed s/real//`
-echo "n=6: $TEST"
 
+set_START
+TEST=`./knight3_6 |tail -1 | sed 's/total //'`
+get_ELAPS
+if [[ "$TEST" -ne 0  ]]; then
+    echo "n=6: FAILED"; exit 1
+else
+    echo "n=6: PASSED. elapsed time: $ELAPS ms"
+fi
+
+gcc ${CFLAGS} -s -Ofast -o knight3_7 knight3.c -lpthread
+echo "To test n=7, execute ./knight3_7 "
